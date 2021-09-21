7adac9738893778dc30f1d2f01849f304ade457a
Ian <iansedano@gmail.com>
2021-09-21 11:28:20 +0200

    updating gameinforeducer

--- src/Reducers/gameInfoReducer.js
+++ src/Reducers/gameInfoReducer.js
@@ -1,7 +1,33 @@
-function gameInfoReducer(state, action) {
+function updateGeneralKnowledgeGame(state, action) {
 	switch (action.type) {
+		case "updateSettings":
+			return {
+				...state,
+				games: {
+					...state.games,
+					generalKnowledge: {
+						...state.games.generalKnowledge,
+						settings: {
+							...state.games.generalKnowledge.settings,
+							...action.payload,
+						},
+					},
+				},
+			};
+		default:
+			return new Error("invalid general knowledge game action");
+	}
+}
+
+function gameInfoReducer(state, action) {
+	const splitAction = action.type.split("/");
+	const actionWithShortenedType = {
+		...action,
+		type: splitAction.slice(1).join("/"),
+	};
+	switch (splitAction[0]) {
 		case "generalKnowledgeGame":
-			return updateGeneralKnowledgeGame(state, action);
+			return updateGeneralKnowledgeGame(state, actionWithShortenedType);
 		default:
 			return new Error("not valid game name");
 	}
