fe878ccc05382cd1e1ebe6bddf6436d7fba8d8ab
Ian <iansedano@gmail.com>
2021-09-22 08:07:53 +0200

    css, and labels

--- src/Components/FormDataListInput.js
+++ src/Components/FormDataListInput.js
@@ -1,9 +1,17 @@
 function FormDataListInput(props) {
+	const validator = (e) => {
+		if (!props.options.includes(e.target.value)) {
+			e.target.style.backgroundColor = "pink";
+		} else e.target.style.backgroundColor = "";
+	};
 	return (
 		<>
+			<label htmlFor={`${props.name}Choice`}>{props.children}</label>
 			<input
 				list={props.name}
+				name={`${props.name}Choice`}
 				defaultValue={props.value === null ? "" : props.value}
+				onBlur={validator}
 			/>
 			<datalist id={props.name}>
 				{props.options.map((option) => (
