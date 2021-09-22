6ecc94f37ffd0ac54082dde34e30fd7afed08252
Ian <iansedano@gmail.com>
2021-09-22 09:59:07 +0200

    starting quiz building

--- src/Games/GeneralKnowledgeGame/Game.js
+++ src/Games/GeneralKnowledgeGame/Game.js
@@ -1,4 +1,37 @@
+import { useState, useEffect } from "react";
+
+import Question from "./Question";
+
+function urlBuilder(difficulty, category, numberOfQuestions) {
+	const root = "https://opentdb.com/api.php?";
+	const params = [
+		difficulty ? `difficulty=${difficulty}` : "",
+		category ? `category=${category}` : "",
+		numberOfQuestions ? `amount=${numberOfQuestions}` : "",
+	];
+	return root + params.join("");
+}
+
 function Game({ difficulty, category, numberOfQuestions }) {
-	return <h3>Game Placeholder</h3>;
+	const [questions, setQuestions] = useState(["making request"]);
+
+	useEffect(() => {
+		async function req() {
+			const resp = await fetch(
+				urlBuilder(difficulty, category, numberOfQuestions)
+			);
+			const json = await resp.json();
+			setQuestions(json.results);
+		}
+		req();
+	}, [difficulty, category, numberOfQuestions]);
+
+	return (
+		<>
+			{questions.map((q, i) => {
+				return <Question key={i}>{q.question}</Question>;
+			})}
+		</>
+	);
 }
 export default Game;
---
+++ src/Games/GeneralKnowledgeGame/Question.js
@@ -0,0 +1,4 @@
+function Question(props) {
+	return <h3>{props.children}</h3>;
+}
+export default Question;
