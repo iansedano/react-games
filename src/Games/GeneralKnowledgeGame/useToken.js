573fd1c3d15f6ece672919fa946e86b641146751
Ian <iansedano@gmail.com>
2021-10-21 16:11:36 +0200

    useToken refactored to use new hooks

--- src/Games/GeneralKnowledgeGame/useToken.js
+++ src/Games/GeneralKnowledgeGame/useToken.js
@@ -1,43 +1,22 @@
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
+
+import useOpenTriviaApi from "./useOpenTriviaApi";
 
 function useToken() {
-	const token = useRef();
+	const tokenRef = useRef();
 
-	useEffect(() => {
-		if (!token.current) {
-			(async () => {
-				const resp = await fetch(
-					"https://opentdb.com/api_token.php?command=request"
-				);
-				if (resp.ok) {
-					const json = await resp.json();
-					switch (json.response_code) {
-						case 0:
-							token.current = json.token;
-							break;
-						case 1:
-							throw new Error("No Results");
-						case 2:
-							throw new Error("Invalid Parameter");
-						case 3:
-							throw new Error("Token not found");
-						case 4:
-							throw new Error("Token Empty");
-						default:
-							throw new Error(
-								"something went wrong with the request for a token"
-							);
-					}
-				} else {
-					throw new Error(
-						"something went wrong with the request for a token"
-					);
-				}
-			})();
-		}
-	});
+	const haveToken = Boolean(tokenRef.current);
 
-	return token.current;
+	const { isLoading, error, response } = useOpenTriviaApi(
+		"api_token.php?command=request",
+		{ abort: haveToken }
+	);
+
+	if (!haveToken && !isLoading && !error && response) {
+		tokenRef.current = response.token;
+	}
+
+	return tokenRef.current;
 }
 
 export default useToken;
